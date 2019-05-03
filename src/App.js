e88be1c2f2157767c8dd32844a387cd7f92dd27b
tommt <atombombmount@gmail.com>
2019-05-03 11:26:52 +0545

    styling

--- src/App.js
+++ src/App.js
@@ -35,10 +35,17 @@ class App extends Component {
 
 
   render() {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
     return (
       <div className="App">
         <h1> OUR APPS </h1>
-        <button onClick={this.switchNameHAndler.bind(this, 'JAVASCRIPT')}>Switch Name</button>
+        <button style={style} onClick={this.switchNameHAndler.bind(this, 'JAVASCRIPT')}>Switch Name</button>
         <Person
           name={this.state.persons[0].name}
           age={this.state.persons[0].age}
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,12 +1,13 @@
 import React from 'react';
-
+import './Person.css'
 const person = (props) => {
   return (
-    <div>
+    <div className="Person">
       <p onClick={props.click}>
         I am {props.name} and I am {props.age} {props.children}
-        <input type="text" onChange={props.changed} value={props.name} />
+
       </p>
+      <input type="text" onChange={props.changed} value={props.name} />
     </div>
   );
 }
